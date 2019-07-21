2d84efdc05c3a4ac62aa6322402e614bf6dcf119
Christopher Sexton <cpsexton@gmail.com>
2019-07-21 01:41:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function theBeatlesPlay(musicians, instruments){
   var arr = [];
   for (let i = 0; i <= 3; i++) {
     let arr = musicians[i] + ' plays ' + instruments[i];
- } return arr
+ } return arr.push(i)
 }
 theBeatlesPlay();
