baa781573a3cc44dc65ddd7bd3b26b28ef06f35a
Christopher Sexton <cpsexton@gmail.com>
2019-07-21 01:30:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  var empty = []
+  var arr = []
   for (let i = 0; i <= 3; i++) {
     let empty = (musicians[i] + ' plays ' + instruments[i]);
- empty.push(i);
- }
+ arr.push(i);
+ } console.log(arr)
 }
 theBeatlesPlay();
