925d341e7219a9e7f005768e6d0950daed8aa387
Christopher Sexton <cpsexton@gmail.com>
2019-07-21 01:37:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  var arr = [];
+  var arr = [''];
   for (let i = 0; i <= 3; i++) {
     let arr = musicians[i] + ' plays ' + instruments[i];
      return arr;
