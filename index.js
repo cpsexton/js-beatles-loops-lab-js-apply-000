a51c577e9be92edf0cbdc348a7852441b557dd26
Christopher Sexton <cpsexton@gmail.com>
2019-07-21 01:28:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  var empty = ['']
+  var empty = []
   for (let i = 0; i <= 3; i++) {
     let empty = (musicians[i] + ' plays ' + instruments[i]);
  console.log(empty);
